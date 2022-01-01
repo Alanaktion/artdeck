e54ad4d37b12c1a75be84c4816edf36b28c737f1
Alan Hardman <alanaktion@gmail.com>
2021-12-31 17:01:02 -0700

    Fix route name conflict

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('/works/{work}/tags', WorkTagsController::class);
+Route::apiResource('/works/{work}/tags', WorkTagsController::class)
+    ->names('api.works.tags');
